3428023aa5c859c128c52719f776ab0bd43a51dd
Pavel Gordon <pavelg@spotify.com>
2021-03-11 14:34:24 +0100

    endpoints

--- index.js
+++ index.js
@@ -1,55 +1,100 @@
+// import { PrismaClient } from '@prisma/client'
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const path = require('path')
 const moment = require('moment')
-const { HOST } = require('./src/constants')
+const {HOST} = require('./src/constants')
 const db = require('./src/database')
 
+
+// const prisma = new PrismaClient()
+
+const PrismaClient = require('@prisma/client').PrismaClient
+const prisma = new PrismaClient()
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 const app = express()
-  .set('port', PORT)
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-
+    .set('port', PORT)
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+app.use(cors())
+app.use(bodyParser.json())
 // Static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
+app.get('/', function (req, res) {
+    res.send('Get ready for OpenSea!');
+})
+//get token by id
+app.get('/api/token/:token_id', function (req, res) {
+    const tokenId = parseInt(req.params.token_id)
+    const tokenPromise = prisma.token.findFirst({
+        where: {
+            id: tokenId
+        }
+    })
+    tokenPromise.then(it => res.send(it))
 })
 
-app.get('/api/token/:token_id', function(req, res) {
-  const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
-  const data = {
-    'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
-    },
-    'image': `${HOST}/images/${tokenId}.png`
-  }
-  res.send(data)
+app.post('/api/token', function (req, res) {
+
+    const createdToken = createToken(req.body)
+    createdToken.then(it => res.send(it))
+
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+async function createToken(req) {
+    console.log(req)
+    // ... you will write your Prisma Client queries here
+    return prisma.token.create({
+        data: {
+            id: req.token_id,
+            title: req.title,
+            artist: req.artist,
+            spotify_uri: req.spotify_uri,
+            image: req.image,
+        },
+    })
+
+    // return prisma.token.findFirst({
+    //     where: {
+    //         id: req.token_id
+    //     }
+    // })
+}
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+    const allUsers = await prisma.token.findMany()
+    console.log(allUsers)
+    return allUsers
+}
+
+main()
+    .catch(e => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
+
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 })
 
 // returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
 function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
+    var zodiac = ['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+    var last_day = ['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+    return (day > last_day[month]) ? zodiac[month * 1 + 1] : zodiac[month];
 }
 
 function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+    return monthNames[month - 1]
 }
